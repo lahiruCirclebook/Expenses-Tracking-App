8cbc060ae73d729d3dd17381993410f49765b168
chandima1998 <igichandima@gmail.com>
2025-05-22 16:21:09 +0530

    first commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Expense_Tracker.Models;
+
+namespace Expense_Tracker.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Category
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+        // GET: Category/AddOrEdit
+        public IActionResult AddOrEdit(int id=0)
+        {
+            if (id != 0)
+            {
+                var category = _context.Categories.Find(id);
+                if (category != null)
+                {
+                    return View(category);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                return View(new Category());
+            }
+        }
+
+        // POST: Category/AddOrEdit
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("CategoryId,Title,Icon,Type")] Category category)
+        {
+            
+            if (category == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                if (category.CategoryId != 0)
+                {
+                    _context.Update(category);
+                }
+                else
+                {
+                    _context.Add(category);
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Expense_Tracker.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(50)]
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; } = "";
+
+        [Required(ErrorMessage = "Icon is required.")]
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";
+
+        [Required(ErrorMessage = "Type is required.")]
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type { get; set; } = "Expense";
+
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
+    }
+}
